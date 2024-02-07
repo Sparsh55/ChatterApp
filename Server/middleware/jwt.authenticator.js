4d4705fa97e498772b02f9848f97ed356bc2c224
Sparsh55 <sparsh9654@gmail.com>
2024-02-07 14:24:02 +0530

    first commit

---
+++ Server/middleware/jwt.authenticator.js
@@ -0,0 +1,22 @@
+import jsonwebtoken from "jsonwebtoken";
+
+const jwtAuth = (req, res, next) => {
+  const token = req.headers["authorization"];
+  console.log(token);
+
+  if (!token) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  try {
+    const payload = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+    req.username = payload.username;
+    req.imageUrl = payload.imageUrl;
+  } catch (err) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  next();
+};
+
+export default jwtAuth;
